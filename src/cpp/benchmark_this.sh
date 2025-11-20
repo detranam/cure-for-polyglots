6922da1d10aa94a5cc4a60e40a5f167515f5f725
detranam <matthew.detra@gmail.com>
2025-11-19 19:21:05 -0600

    Ensure CPP and Go are benchmarking correctly, outputting files into results dir

--- src/cpp/benchmark_this.sh
+++ src/cpp/benchmark_this.sh
@@ -1,19 +1,14 @@
 lsDirectories="$(ls -d */)"
 directories=($lsDirectories)
+cp ../../test-materials/testwords.txt ./testwords.txt
 
-for file in *.cpp; do
-    file="dynamic_array_with_allocated.cpp"
+for file in *.cpp; do    
     executableName=${file::-4}
     echo Benchmarking C++ program $executableName
-    # Preparatory things
-    cmake -Bbuild -S.
-    cp ../../test-materials/testwords.txt build/
-    rm -rf build
     # Benchmark the building without caching
-    hyperfine --export-json ${executableName}_build.json --prepare 'rm -rf build'  "cmake -D${executableName}=1 -Bbuild -S. && cmake --build build"
-    # Benchmark the running.
-    cmdbench -i 10 -s build/${executableName} > ${executableName}_runtime.txt
-    exit 1
+    hyperfine --export-json results/${executableName}_build_without_caching.json --prepare 'rm -rf build'  "cmake -D${executableName}=1 -Bbuild -S. && cmake --build build"
+    # We now can assume the build exists, test building with caching (effectively doing nothing)
+    hyperfine --export-json results/${executableName}_build_with_caching.json "cmake -D${executableName}=1 -Bbuild -S. && cmake --build build"
+    # Benchmark the running
+    cmdbench -i 10 -s build/${executableName} > results/${executableName}_runtime.txt
 done
-
-cp ../../test-materials/testwords.txt print_testwords/
--- src/go/benchmark_this.sh
+++ src/go/benchmark_this.sh
@@ -1,5 +1,8 @@
 lsDirectories="$(ls -d */)"
 directories=($lsDirectories)
+cp ../../test-materials/testwords.txt print_testwords/
+rm -rf results
+mkdir results
 
 for dir in "${directories[@]}"
 do
@@ -9,11 +12,14 @@ do
     go fmt
     go clean -cache
     # Benchmark the building without caching
-    hyperfine --export-json ${dir::-1}_build.json --prepare 'go clean -cache'  'go build'
+    hyperfine --export-json ${dir::-1}_build_without_caching.json --prepare 'go clean -cache'  'go build'
+    # Benchmark the building with caching
+    hyperfine --export-json ${dir::-1}_build_with_caching.json 'go build'
     # Benchmark the running.
     echo "cmdbench -i 10 -s ./${dir::-1} > ${dir::-1}_runtime.txt"
     cmdbench -i 10 -s ${dir::-1} > ${dir::-1}_runtime.txt
     cd ..
 done
 
-cp ../../test-materials/testwords.txt print_testwords/
+find ./ -name '*.json' -exec cp -prv '{}' 'results' ';'
+find ./ -name '*.txt' -exec cp -prv '{}' 'results' ';'
